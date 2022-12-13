1f119b3ca07c68500a7a679be81bd80e268eae31
Tao Xiang <xiang.tao@outlook.de>
2022-12-13 07:42:13 +0100

    1

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -1,26 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-  });
-
-  win.loadFile('index.html');
-};
-
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-});
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+  ipcMain.handle('ping', () => 'pong')
+  win.loadFile('index.html')
+}
+app.whenReady().then(createWindow)
